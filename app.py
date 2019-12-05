0b5b56750f436e7153849a6e03c33a10709c7db7
Breyanna Vass <breyannavass@gmail.com>
2019-12-05 17:50:48 -0500

    made a table that allows for GET and POST

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
