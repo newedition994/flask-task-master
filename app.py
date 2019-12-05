7f5ab8630936bf9db20405b9c5cb66389f65832f
Breyanna Vass <breyannavass@gmail.com>
2019-12-05 17:35:24 -0500

    adjusting file layout and finishing the database layout

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 @app.route('/')
 def index():
     return render_template('index.html')
