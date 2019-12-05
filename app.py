38ebcafadf172ee210f92dd8da359c8774be8d63
Breyanna Vass <breyannavass@gmail.com>
2019-12-05 18:05:43 -0500

    set up the table properly

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        tasks = Task.query.order_by(Todo.date_created).all()
+        tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
